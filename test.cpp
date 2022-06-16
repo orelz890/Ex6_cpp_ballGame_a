78a6abd7ce43542d994baf0765d17f91d447500c
orelz890 <93476230+orelz890@users.noreply.github.com>
2022-06-16 23:11:13 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,6 +4,8 @@
 #include "sources/Schedule.hpp"
 #include "sources/Score.hpp"
 #include "sources/Team.hpp"
+#include "sources/iterator.hpp"
+
 
 #include <string>
 #include <iostream>
@@ -17,33 +19,38 @@ using namespace std;
 TEST_CASE("Good tests"){
     cout << "========================================== Start testing ==========================================\n";
 
-    /* ==================
-     * Constructor check
-     * ==================*/
+    /* ============
+     * Init check
+     * ============*/
+    SUBCASE("Init check"){
+        Leauge* leauge = new Leauge();
+        Team* team;
+        std::vector<std::string> names;
+        for (int i = 0; i < TEAMS_SIZE; i++)
+        {
+            team = &(*leauge)[i];
+            cout << "Name:" << team->get_name() <<" Losses: " << team->get_losses() << " winings: " << team->get_wins() << " skill lvl: "<<  team->get_skill_lvl() << "\n";
+            // Verify all the names are diffrent
+            assert(find(names.begin(), names.end(), team->get_name()) == names.end());
+            assert(team->get_losses() == 0 && team->get_wins() == 0 && team->get_skill_lvl() != 0);
+        }
 
-    Leauge* leauge = new Leauge();
-    Team* team;
-    std::vector<std::string> names;
-    for (int i = 0; i < TEAMS_SIZE; i++)
-    {
-        team = &(*leauge)[i];
-        cout << "Name:" << team->get_name() <<" Losses: " << team->get_losses() << " winings: " << team->get_wins() << " skill lvl: "<<  team->get_skill_lvl() << "\n";
-        // Verify all the names are diffrent
-        assert(find(names.begin(), names.end(), team->get_name()) == names.end());
-        assert(team->get_losses() == 0 && team->get_wins() == 0 && team->get_skill_lvl() != 0);
-    }
+        Schedule* schedule = new Schedule(leauge);
+        for (int i = 0; i < TEAMS_SIZE; i++)
+        {
+            assert((*leauge)[i] == (*schedule->get_leauge())[i]);
+        }
+        assert(schedule->get_games_counter() == 380);
+        cout << "There where " << schedule->get_games_counter() << " games\n";
+        Score* score = new Score(leauge, schedule);
 
-    Schedule* schedule = new Schedule(leauge);
-    for (int i = 0; i < TEAMS_SIZE; i++)
-    {
-        assert((*leauge)[i] == (*schedule->get_leauge())[i]);
-    }
-    assert(schedule->get_games_counter() == 380);
-    cout << "There where " << schedule->get_games_counter() << " games\n";
+        for (auto i = leauge->begin(); i != leauge->end(); ++i)
+        {
+            std::cout << *i << '\n';
+        }
+        
+        
+    }// Init check
 
-    Score* score = new Score(leauge, schedule);
-
-    cout << score->longest_losses_in_a_row() << '\n';
-    // schedule->print_games_schedule();
-
+    // SUBCASE(""){}
 }
