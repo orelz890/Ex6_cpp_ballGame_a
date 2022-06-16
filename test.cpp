a791f5d5600748982beb7eaae586a9aab855e5bb
orelz890 <93476230+orelz890@users.noreply.github.com>
2022-06-16 21:04:25 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,50 @@
+// #include "doctest.h"
+#include "Game.hpp"
+#include "Leauge.hpp"
+#include "Schedule.hpp"
+#include "Score.hpp"
+#include "Team.hpp"
+
+#include <string>
+#include <iostream>
+#include <assert.h>
+#include <vector>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main(){
+    cout << "========================================== Start testing ==========================================\n";
+
+    /* ==================
+     * Constructor check
+     * ==================*/
+
+    Leauge* leauge = new Leauge();
+    Team* team;
+    std::vector<std::string> names;
+    for (int i = 0; i < TEAMS_SIZE; i++)
+    {
+        team = &(*leauge)[i];
+        cout << "Name:" << team->get_name() <<" Losses: " << team->get_losses() << " winings: " << team->get_wins() << " skill lvl: "<<  team->get_skill_lvl() << "\n";
+        // Verify all the names are diffrent
+        assert(find(names.begin(), names.end(), team->get_name()) == names.end());
+        assert(team->get_losses() == 0 && team->get_wins() == 0 && team->get_skill_lvl() != 0);
+    }
+
+    Schedule* schedule = new Schedule(leauge);
+    for (int i = 0; i < TEAMS_SIZE; i++)
+    {
+        assert((*leauge)[i] == (*schedule->get_leauge())[i]);
+    }
+    assert(schedule->get_games_counter() == 380);
+    cout << "There where " << schedule->get_games_counter() << " games\n";
+
+    Score* score = new Score(leauge, schedule);
+
+    cout << score->longest_losses_in_a_row() << '\n';
+    // schedule->print_games_schedule();
+
+    return 0;
+}
